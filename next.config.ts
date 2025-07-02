393928d5f09c97dd9ccdd2c8173427593895eb57
Akshit Thakur <thxkursxhxb@gmail.com>
2025-07-02 22:22:38 +0530

    deploy

--- next.config.ts
+++ next.config.ts
@@ -1,7 +1,15 @@
-import type { NextConfig } from "next";
+/** @type {import('next').NextConfig} */
+const nextConfig = {
+  // 1. A dd this to produce a static export
+  output: 'export',
 
-const nextConfig: NextConfig = {
-  /* config options here */
+  // 2. Add the base path
+  basePath: '/game-of-life',
+
+  // 3. Disable image optimization (required for static export)
+  images: {
+    unoptimized: true,
+  },
 };
 
 export default nextConfig;
